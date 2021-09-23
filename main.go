607e87e67f328604ffe756c9744dcb80e034a525
Vitor Gouveia <vitorbgouveia@gmail.com>
2021-09-22 22:49:52 -0300

    create file main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package goarea
+
+import "math"
+
+const Pi = 3.1416
+
+func Circ(raio float64) float64  {
+	return math.Pow(raio, 2) * Pi
+}
+
+func Rect(base, altura float64) float64  {
+	return base * altura
+}
+
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+}
